58a0fec3b422ed57df91b80e0a8c93fbfb14dd8f
Phil Haines <phil.haines92@gmail.com>
2021-02-03 09:02:17 -0700

    commit tree f5cc6f31e6f76ec773455be54131d16d04288c4a

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+
+    if num <= 1
+        return false
+    elsif num > 0 
+        (2..(num - 1)).each do |n|
+            if num % n == 0
+                return false
+            end
+        end
+    end
+  true
+end
